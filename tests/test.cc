2d420562b0681d3f434efce1b492437be4ca5650
dthuang3 <dthuang3@illinois.edu>
2020-05-06 11:42:52 -0700

    Edit unit tests

--- tests/test.cc
+++ tests/test.cc
@@ -18,23 +18,24 @@ TEST_CASE("Is Topped Out", "[Game]") {
   SECTION("Start of game") {
     REQUIRE_FALSE(game->IsToppedOut());
   }
-  SECTION("After running for 10000 frames") {
+  SECTION("After running for 100000 frames") {
     // run world for 10000 frames at 60 frames per iteration
-    for (size_t i = 0; i < 10000; i++) {
+    for (size_t i = 0; i < 100000; i++) {
       game->Update();
     }
     REQUIRE(game->IsToppedOut());
   }
+  delete game;
 }
 
 TEST_CASE("Get Score", "[Game]") {
   Game* game = new Game();
   REQUIRE(game->GetScore() == 0);
+  delete game;
 }
 
 TEST_CASE("Hold Current Piece", "[Game]") {
   Game* game = new Game();
-  
   SECTION("Beginning nothing is held") {
     REQUIRE(game->GetHeldType() == TetrominoPieceType::Empty);
   }
@@ -46,34 +47,36 @@ TEST_CASE("Hold Current Piece", "[Game]") {
     REQUIRE(game->GetHeldType() != TetrominoPieceType::Empty);
     REQUIRE(game->GetHeldType() == to_be_held);
   }
+  delete game;
 }
 
 TEST_CASE("Get Next Type", "[Game]") {
   Game* game = new Game();
   REQUIRE(game->GetNextType() != TetrominoPieceType::Empty);
+  delete game;
 }
 
 TEST_CASE("Update game", "[Game]") {
   Game* game = new Game();
-  
+
   SECTION("Beginning of game") {
     REQUIRE(game->GetScore() == 0);
     REQUIRE(game->GetHeldType() == TetrominoPieceType::Empty);
   }
-  
+
   SECTION("Update game one step") {
     game->Update();
     REQUIRE(game->GetScore() == 1);
     REQUIRE(game->GetHeldType() == TetrominoPieceType::Empty);
   }
-  
-  SECTION("Update game one and hold piece") {
-    TetrominoPieceType to_be_held = game->GetCurrentPiece()->GetType();
+
+  SECTION("Update game one step and hold piece") {
+    TetrominoPieceType type_to_be_held = game->GetCurrentPiece()->GetType();
     game->HoldCurrentPiece();
     game->Update();
     REQUIRE(game->GetScore() == 1);
     REQUIRE(game->GetHeldType() != TetrominoPieceType::Empty);
-    REQUIRE(game->GetHeldType() == to_be_held);
+    REQUIRE(game->GetHeldType() == type_to_be_held);
   }
+  delete game;
 }
-
